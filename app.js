bb31f9fdffef864df55eed39c803e7612e5274f1
verteramo.federico <verteramo.federico@gmail.com>
2021-06-01 20:00:03 -0300

    modifico app.js puertos

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.listen(3000, () => console.log("Server Start in http://localhost:3000"))
+/* app.listen(3000, () => console.log("Server Start in http://localhost:3000")) */
+
+app.set("port", process.env.PORT ||3000)
+
+app.listen(app.get("port"), () => console.log("Server Start in http://localhost:3000"))
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
 app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")))
