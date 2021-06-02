afd4660b9343b9d0cddc881e95bd24e2d92b6c36
Gullermokr <guillotekri@gmail.com>
2021-06-01 20:36:40 -0700

    correccion de nombres de productos

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-<<<<<<< HEAD
-app.listen(3030, () => console.log("Server Start in http://localhost:3030"))
-=======
 /* app.listen(3000, () => console.log("Server Start in http://localhost:3000")) */
 
 app.set("port", process.env.PORT ||3000)
 
 app.listen(app.get("port"), () => console.log("Server Start in http://localhost:3000"))
->>>>>>> 0dda56f1c3ac522de56ac8e32b8fa6648d4bc3db
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
 app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")))
