2fb4eb31e356625570340d27033376b89497c563
verteramo.federico <verteramo.federico@gmail.com>
2021-06-04 11:41:11 -0300

    seccion nosotros

--- app.js
+++ app.js
@@ -13,3 +13,4 @@ app.listen(app.get("port"), () => console.log("Server Start in http://localhost:
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
 app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")))
 app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")))
+app.get("/nosotros", (req, res) => res.sendFile(path.join(__dirname, "views", "nosotros.html")))
