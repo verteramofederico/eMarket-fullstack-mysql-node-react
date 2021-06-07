df2d2cc1cd83b96b35987c150d35ba03ae2d8771
verteramo.federico <verteramo.federico@gmail.com>
2021-06-07 15:13:20 -0300

    agregago pag descripcion producto

--- app.js
+++ app.js
@@ -14,3 +14,4 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html
 app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")))
 app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")))
 app.get("/nosotros", (req, res) => res.sendFile(path.join(__dirname, "views", "nosotros.html")))
+app.get("/productDetail", (req, res) => res.sendFile(path.join(__dirname, "views", "productDetail.html")))
