4c5e67f92b5c560eb183ff6ad66b8e42fadeebb6
verteramo.federico <verteramo.federico@gmail.com>
2021-08-18 11:03:15 -0300

    scripts para correcto funcionamiento

---
+++ database/scripts/colorsProducts.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: SELECT * FROM gaf_db.colorsProducts
+LIMIT 0, 1000
+
+-- Date: 2021-08-18 10:57
+*/
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (1,1,1);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (2,1,2);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (3,2,2);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (4,2,3);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (5,3,1);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (6,3,2);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (7,4,4);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (8,4,1);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (9,5,2);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (10,6,1);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (11,7,1);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (12,7,4);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (13,8,1);
+INSERT INTO `colorsProducts` (`id`,`productId`,`colorsId`) VALUES (14,8,2);
+
