783a70f022b507b168dbdeba9dcb00ca8f1b41f1
verteramo.federico <verteramo.federico@gmail.com>
2021-08-23 20:25:50 -0300

    validacion crear producto front

---
+++ public/js/productCreateOrEdit.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+    console.log("validando")
+
+    let form = document.querySelector("form")
+
+    let name = document.querySelector("#name")
+    let description = document.querySelector("#description")
+    let image = document.querySelector("#image")
+
+    console.log(name,description,image);
+
+    name.focus()
+    form.addEventListener("submit", (e) => {
+
+        // name validator
+        if (name.value === "" || name.value.length < 2) {
+            name.classList.add("is-invalid")
+                e.preventDefault()
+                let divError = document.querySelector("#nameError")
+                divError.innerHTML += "El campo nombre debe contener al menos 2 caracteres"             
+        } 
+
+        // domicilio validator
+        if (description.value === "" || description.value.length < 15) {
+            description.classList.add("is-invalid")
+                e.preventDefault()
+                let divError = document.querySelector("#descriptionError")
+                divError.innerHTML += "Descripcion debe contener al menos 15 caracteres"
+               // alert("Debes ingresar un domicilio");
+                
+        }
+        // image validator
+        var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+        if (image.value === "" || !allowedExtensions.exec(image.value) ) {
+            image.classList.add("is-invalid")
+                e.preventDefault()
+                let divError = document.querySelector("#imageError")
+                divError.innerHTML += "Debes ingresar imagen jpg, jpge, gif o png"
+             //   alert("Debes ingresar imagen jpg, jpge, gif o png");
+                
+        } 
+    })
+
+
+
+}
