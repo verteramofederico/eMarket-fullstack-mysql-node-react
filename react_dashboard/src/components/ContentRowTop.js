ff0b1f4a3f3288d1db9eb0eda54f5a8989cbd7c0
verteramo.federico <verteramo.federico@gmail.com>
2021-09-18 15:32:48 -0300

    react clean, ready to consume API

---
+++ react_dashboard/src/components/ContentRowTop.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import CardInfo from "./CardInfo"
+
+function ContentRowTop(){
+    return(
+        <React.Fragment>
+				{/*<!-- Content Row Top -->*/}
+				<div className="container-fluid">
+					<div className="d-sm-flex aligns-items-center justify-content-between mb-4">
+						<h1 className="h3 mb-0 text-gray-800  ">GAF Textiles App Dashboard </h1>
+					</div>
+				
+					{/*<!-- Content Row Movies-->*/}
+					<div className="row">
+
+						{/*<!-- Movies in Data Base -->*/}
+						<CardInfo title="Movies in database" total={20} icon="fa-film"/>
+
+						{/*<!-- Total awards -->*/}
+						<CardInfo title="Total awards" total={79} icon="fa-award" color="warning"/>
+
+						{/*<!-- Actors quantity -->*/}
+						<CardInfo title="Actors" total={2490} icon="fa-user"/>
+						<CardInfo  total="2490"/>
+					</div>
+					{/*<!-- End movies in Data Base -->*/}
+					
+	
+					{/*<!-- Content Row Last Movie in Data Base -->*/}
+					<div className="row">
+						{/*<!-- Last Movie in DB -->*/}
+						<div className="col-lg-6 mb-4">
+							<div className="card shadow mb-4">
+								<div className="card-header py-3">
+									<h5 className="m-0 font-weight-bold text-gray-800">Last movie in Data Base</h5>
+								</div>
+								<div className="card-body">
+									
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
+									<a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
+								</div>
+							</div>
+						</div>
+						{/*<!-- End content row last movie in Data Base -->*/}
+
+						{/*<!-- Genres in DB -->*/}
+						<div className="col-lg-6 mb-4">						
+							<div className="card shadow mb-4">
+								<div className="card-header py-3">
+									<h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
+								</div>
+								<div className="card-body">
+									<div className="row">
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Acción
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Animación
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Aventura
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Ciencia Ficción
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Comedia
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Documental
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Drama
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Fantasia
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Infantiles
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Musical
+												</div>
+											</div>
+										</div>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				{/*<!--End Content Row Top-->*/}
+
+        </React.Fragment>
+    )
+
+}
+export default ContentRowTop;
--- react_dashboard/src/index.js
+++ react_dashboard/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import './assets/css/app.css';
 
 
 ReactDOM.render(
@@ -9,3 +10,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
